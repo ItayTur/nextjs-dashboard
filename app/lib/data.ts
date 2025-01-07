9ebde269950bd731daeab90e6eb6a9a6dc8b0532
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2025-01-07 17:52:10 +0200

    feat: implements streaming
    1) Loading.tsx
    2) Suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,13 +9,17 @@ import {
 } from "./definitions";
 import { formatCurrency } from "./utils";
 
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
     console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await delay(1500);
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -36,7 +40,8 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    console.log("waiting for latest invoices");
+    await delay(3000);
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
