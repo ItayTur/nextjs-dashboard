b40fad91a85dc173ac3b7ce0c52dd097da7fe047
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2025-01-18 19:00:37 +0200

    fixes pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const onSearch = useDebouncedCallback((search: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (search) {
       params.set("query", search);
     } else {
