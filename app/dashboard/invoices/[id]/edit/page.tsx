62e09bd4adfac611b45b4569ff844032f67c9471
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2025-01-26 18:11:31 +0200

    feat: updates invoice by id

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,27 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
+
+export default async function Invoice({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          { label: "Invoice", href: "/dashboard/invoices/edit", active: true },
+        ]}
+      />
+      <Form customers={customers} invoice={invoice} />
+    </main>
+  );
+}
